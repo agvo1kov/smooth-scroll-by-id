2b45a484620c76b4e63ac19c394a0d3d337164e6
Alexander Volkov <agvolkov5@gmail.com>
2019-03-05 23:01:27 +0300

    done

---
+++ script.js
@@ -0,0 +1,47 @@
+const a = document.getElementsByTagName('a');
+console.log(a);
+
+for (let i = 0; i < a.length; i++) {
+	a[i].addEventListener('click', function(event) {
+		event.preventDefault(); // abort <a> click
+		const element = document.querySelector(a[i].getAttribute('href')); // get the signed element
+	
+		const scrollYStart = window.scrollY;
+		const scrollOffset = element.offsetTop - scrollYStart;
+
+		console.log(scrollOffset);
+
+		animate({
+            duration: Math.abs(scrollOffset) * 0.5, // sets same scrolling speed for different scroll offset (change to constant for same scrolling time)
+            timing: easeInOutQuad,
+            draw: function(progress) {
+                window.scrollTo(0, scrollYStart + progress * scrollOffset);
+            }
+        });
+	});
+}
+
+function animate(options) {
+
+  let start = performance.now();
+
+  requestAnimationFrame(function animate(time) {
+    // timeFraction от 0 до 1
+    let timeFraction = (time - start) / options.duration;
+    if (timeFraction > 1) timeFraction = 1;
+
+    // текущее состояние анимации
+    let progress = options.timing(timeFraction);
+
+    options.draw(progress);
+
+    if (timeFraction < 1) {
+      requestAnimationFrame(animate);
+    }
+
+  });
+}
+
+function easeInOutQuad(progress) {
+	return progress < .5 ? 2*progress*progress : -1+(4-2*progress)*progress
+}
