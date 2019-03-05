7ead3aeb08eaf9122b93215e19f1f72cbb6f1589
Alexander Volkov <agvolkov5@gmail.com>
2019-03-05 23:10:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -26,11 +26,11 @@ function animate(options) {
   let start = performance.now();
 
   requestAnimationFrame(function animate(time) {
-    // timeFraction от 0 до 1
+    // timeFraction from 0 to 1
     let timeFraction = (time - start) / options.duration;
     if (timeFraction > 1) timeFraction = 1;
 
-    // текущее состояние анимации
+    // current animate state
     let progress = options.timing(timeFraction);
 
     options.draw(progress);
